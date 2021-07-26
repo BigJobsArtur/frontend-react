65d72759e684b87760b7b1cc6458be4245ca6a0b
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-07-26 18:42:10 -0300

    Conclusão do desafi 😁

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,28 +1,50 @@
 import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+
+import prevarrow from "../../assets/img/prevarrow.svg";
+import nxtarrow from "../../assets/img/nxtarrow.svg";
 
 import "./Table.scss";
 
-interface tableData {
+interface TableData {
   id: string;
   user_id: string;
   title: string;
   body: string;
 }
 
+interface Pagination {
+  total: number;
+  pages: number;
+  page: number;
+  limit: number;
+}
+
 const Table = () => {
-  const [post, setPost] = useState<tableData[]>([]);
+  const [post, setPost] = useState<TableData[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagination, setPagination] = useState<Pagination>();
+
+  useEffect(() => {
+    api.get(`posts`).then((response) => {
+      setPost(response.data.data);
+      setPagination(response.data.meta.pagination);
+    });
+  }, []);
 
   // useEffect(() => {
-  //   api.get("posts").then((response) => setPost(response.data));
+  //   fetch("https://gorest.co.in/public/v1/posts")
+  //     .then((response) => response.json())
+  //     .then((res) => setPost(res.data));
   // }, []);
 
   useEffect(() => {
-    fetch("https://gorest.co.in/public/v1/posts")
-      .then((response) => response.json())
-      .then((res) => setPost(res.data));
-  }, []);
+    api.get(`posts?page=${currentPage}`).then((response) => {
+      setPost(response.data.data);
+      setPagination(response.data.meta.pagination);
+    });
+  }, [currentPage]);
 
-  console.log(post);
   return (
     <section>
       <div>
@@ -30,35 +52,61 @@ const Table = () => {
         <table>
           <thead>
             <tr>
-              <th>titulo</th>
-              <th>conteúdo</th>
+              <th>Título</th>
+              <th>Conteúdo</th>
             </tr>
           </thead>
-          {
-            // <thead>
-            //   {head.map((item) => (
-            //     <tr>
-            //       <th>{item}</th>
-            //     </tr>
-            //   ))}
-            // </thead>
-          }
-
-          {/* <tbody>
-            {post.map((i) => (
-              <tr key={i.id}>
-                <td>{i.title}</td>
-                <td>{i.body}</td>
-              </tr>
-            ))}
-          </tbody> */}
           <tbody>
             {post.map((i) => (
-              <tr>
-                <td>{i.title}</td>
-                <td>{i.body}</td>
+              <tr key={i.id}>
+                <td className="title">
+                  <p className="m-0">{i.title}</p>
+                </td>
+                <td className="posts">{i.body}</td>
               </tr>
             ))}
+
+            <tr className="pagination-count">
+              <td>
+                {`Exibindo ${pagination ? pagination.limit : 0} postagens `}
+              </td>
+              <td>
+                {/* Prevarrow */}
+                {pagination && pagination.page > 1 && (
+                  <button
+                    onClick={() => setCurrentPage((prevState) => prevState - 1)}
+                  >
+                    <img src={prevarrow} alt="" />
+                  </button>
+                )}
+              </td>
+
+              {/* Numbers */}
+              <td className="pagination-numbers ">
+                {pagination && pagination.page > 1 && (
+                  <button>{pagination.page - 1}</button>
+                )}
+
+                <button className="current_page">
+                  {pagination && pagination.page}
+                </button>
+
+                {pagination && pagination.page < pagination.pages && (
+                  <button>{pagination.page + 1}</button>
+                )}
+              </td>
+
+              {/* Nextarrow */}
+              <td>
+                {pagination && pagination.page < pagination.pages && (
+                  <button
+                    onClick={() => setCurrentPage((prevState) => prevState + 1)}
+                  >
+                    <img src={nxtarrow} alt="" />
+                  </button>
+                )}
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL:"https://gorest.co.in/public/v1/"
-})
-
-export default api
